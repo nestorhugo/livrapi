396d589efbbc2d77291885e880816da0c5adf73e
Nestor <nestorklauck@gmail.com>
2023-07-28 15:58:03 -0300

    Hidden Conncet

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+
+const connect = process.env.CHAVE;
 
 mongoose.set("strictQuery", true);
 
-mongoose.connect(
-  "mongodb+srv://nestorklauck:123@cluster0.htnt5.mongodb.net/alura-node"
-);
+mongoose.connect(connect);
 
 let db = mongoose.connection;
 
