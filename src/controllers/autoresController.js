1685bb4a28114f3f650c6cd3eab553cbdb539287
Nestor <nestorklauck@gmail.com>
2023-07-31 14:37:01 -0300

    Express Middlewares

--- src/controllers/autoresController.js
+++ src/controllers/autoresController.js
@@ -1,4 +1,3 @@
-import mongoose from "mongoose";
 import autores from "../models/Autor.js";
 
 class AutorController {
@@ -12,7 +11,7 @@ class AutorController {
     }
   };
 
-  static listarAutorPorID = async (req, res) => {
+  static listarAutorPorID = async (req, res, next) => {
     try {
       const id = req.params.id;
 
@@ -24,17 +23,11 @@ class AutorController {
         res.status(404).send({ message: "Id do Autor não localizado." });
       }
     } catch (erro) {
-      if (erro instanceof mongoose.Error.CastError) {
-        res
-          .status(400)
-          .send({ message: "Um ou mais dados fornecidos estão incorretos." });
-      } else {
-        res.status(500).send({ message: "Erro interno de servidor." });
-      }
+      next(erro);
     }
   };
 
-  static cadastrarAutor = async (req, res) => {
+  static cadastrarAutor = async (req, res, next) => {
     try {
       let autor = new autores(req.body);
 
@@ -42,13 +35,11 @@ class AutorController {
 
       res.status(201).send(autorResultado.toJSON());
     } catch (erro) {
-      res
-        .status(500)
-        .send({ message: `${erro.message} - falha ao cadastrar Autor.` });
+      next(erro);
     }
   };
 
-  static atualizarAutor = async (req, res) => {
+  static atualizarAutor = async (req, res, next) => {
     try {
       const id = req.params.id;
 
@@ -56,11 +47,11 @@ class AutorController {
 
       res.status(200).send({ message: "Autor atualizado com sucesso" });
     } catch (erro) {
-      res.status(500).send({ message: erro.message });
+      next(erro);
     }
   };
 
-  static excluirAutor = async (req, res) => {
+  static excluirAutor = async (req, res, next) => {
     try {
       const id = req.params.id;
 
@@ -68,7 +59,7 @@ class AutorController {
 
       res.status(200).send({ message: "Autor removido com sucesso" });
     } catch (erro) {
-      res.status(500).send({ message: erro.message });
+      next(erro);
     }
   };
 }
