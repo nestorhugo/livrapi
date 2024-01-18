ceaeddbd36246dd88acf80f86fa3efc65df679ad
nestorhugo <nestorhugo@proton.me>
2024-01-17 22:55:08 -0300

    Add routes for retrieving and creating books

--- src/app.js
+++ src/app.js
@@ -1,9 +1,37 @@
 import express from "express";
 
 const app = express();
+app.use(express.json());
+
+const livros = [
+  {
+    id: 1,
+    nome: "O Senhor dos Anéis",
+    autor: "J. R. R. Tolkien",
+  },
+  {
+    id: 2,
+    nome: "Harry Potter e a Pedra Filosofal",
+    autor: "J. K. Rowling",
+  },
+  {
+    id: 3,
+    nome: "As Crônicas de Nárnia",
+    autor: "C. S. Lewis",
+  },
+];
 
 app.get("/", (req, res) => {
   res.status(200).send("Buks!");
+});
+
+app.get("/livros", (req, res) => {
+  res.status(200).json(livros);
+});
+
+app.post("/livros", (req, res) => {
+  livros.push(req.body);
+  res.status(201).json(livros[livros.length - 1]);
 });
 
 export default app; // Exporta o app para ser usado em outros arquivos
