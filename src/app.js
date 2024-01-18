014eeb80ab80a7569067187abb2f27d7438e6280
nestorhugo <nestorhugo@proton.me>
2024-01-17 23:32:46 -0300

    Add CRUD operations for books

--- src/app.js
+++ src/app.js
@@ -21,6 +21,12 @@ const livros = [
   },
 ];
 
+function buscaLivro(id) {
+  return livros.findIndex((livro) => {
+    return livro.id === Number(id);
+  });
+}
+
 app.get("/", (req, res) => {
   res.status(200).send("Buks!");
 });
@@ -29,9 +35,34 @@ app.get("/livros", (req, res) => {
   res.status(200).json(livros);
 });
 
+app.get("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  if (index >= 0) {
+    res.status(200).json(livros[index]);
+  } else {
+    res.status(404).send("Livro não encontrado.");
+  }
+});
+
 app.post("/livros", (req, res) => {
   livros.push(req.body);
   res.status(201).json(livros[livros.length - 1]);
 });
 
-export default app; // Exporta o app para ser usado em outros arquivos
+app.put("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  livros[index].nome = req.body.nome;
+  res.status(200).json(livros[index]);
+});
+
+app.delete("/livros/:id", (req, res) => {
+  const index = buscaLivro(req.params.id);
+  if (index >= 0) {
+    livros.splice(index, 1);
+    res.status(204).end();
+  } else {
+    res.status(404).send("Livro não encontrado.");
+  }
+});
+
+export default app;
